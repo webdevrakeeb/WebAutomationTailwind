1317acea59c1994fe77fb687872395b22e3bf46b
webdevrakeeb <developerrakeeb01@gmail.com>
2025-05-09 00:19:59 +0600

    Add Four Sections After Hero Section

--- js/custom.js
+++ js/custom.js
@@ -18,6 +18,44 @@
             jQuery(this).find('.drp-item').slideDown();
         })
 
+        $('.slider').slick({
+            slidesToShow: 8,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 0,
+            speed: 3000,
+            cssEase: 'linear',
+            infinite: true,
+            arrows: false,
+            pauseOnHover: false,
+            responsive: [
+              {
+                breakpoint: 1280,
+                settings: {
+                  slidesToShow: 6
+                }
+              },
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3
+                }
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 2
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 2
+                }
+              }
+            ]
+        });
+
 
 
 
